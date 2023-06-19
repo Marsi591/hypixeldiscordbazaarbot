5d02ae31903bc53f378b0663f22d693287500394
technosplat <supersplatman01@gmail.com>
2023-06-19 12:20:34 -0500

    Cogs

---
+++ Cogs/bazaar_cog.py
@@ -0,0 +1,101 @@
+#Imports
+import discord
+import asyncio
+from discord.ext import commands
+import discord
+from discord.ext import commands, tasks
+import time
+import config
+import hypixel_api
+import random
+
+#Variables
+hypixel = hypixel_api.HypixelApi()
+responses = config.RESPONSES
+bazaar_data = None
+
+#Miscellanious Functions
+def f_num(number, decimals=0):
+    return f"{round(number, decimals):,}"
+
+def f_coins(number):
+    return "$" + f_num(number, decimals=0)
+
+
+
+class BazaarCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.settings = {
+            "margin": 250,
+            "volume": 1000000,
+        }
+        self.embed_list = ()
+
+    @commands.command()
+    async def bz(self, ctx, arg):
+        item = hypixel.items.get_item_by_id(arg)
+        embed = discord.Embed(
+            title=f"You wanted to know about {item.name} ~?",
+            color=0xF5A9B8
+        )
+        embed.set_thumbnail(url=self.bot.user.avatar.url)
+        embed.set_footer(text=time.ctime(time.time()))
+        embed.add_field(
+            name=item.name,
+            value=f"BUY: {f_coins(item.bz_price['buy'])}\nSELL: {f_coins(item.bz_price['sell'])}\n7d SALES: {f_num(item.bz_moving_week['sell'])}"
+        )
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def margin(self, ctx, ammount, t_type):
+        if t_type == "percent":
+            ctx.send(f"Set margin tracker to {ammount}% of the total price.")
+        if t_type == "coins":
+            ctx.send(f"Set margin tracker to {ammount}% coins.")
+
+    @commands.command()
+    async def spike(self, ctx, ammount, t_type):
+        if t_type == "percent":
+            ctx.send(f"Set spike tracker to {ammount}% of the total price.")
+        if t_type == "coins":
+            ctx.send(f"Set spike tracker to {ammount}% coins.")
+
+    @commands.command()
+    async def sell_volume(self, ctx, ammount):
+        ctx.send(f"Set the tacker sell volume to {ammount} sellers.")
+
+    @commands.command()
+    async def buy_volume(self, ctx, ammount):
+        ctx.send(f"Set the tacker buy volume to {ammount} sellers.")
+
+    @commands.command()
+    async def brief(self, ctx):
+        new_search = hypixel.bazaar.limit_search()
+        new_search.add_limit("margin", False, self.settings["margin"])
+        new_search.add_limit("sellVolume", False, self.settings["volume"])
+        results = new_search.finalize()
+        embed = discord.Embed(
+            title=random.choice(responses),
+            color=0xF5A9B8,
+        )
+        embed.set_thumbnail(url=self.bot.user.avatar.url)
+        embed.set_footer(text=time.ctime(time.time()))
+
+        for i in results:
+
+            embed.add_field(
+                name=f"{i.name}",
+                value=f"MARGIN: {f_coins(i.bz_price['margin'])}\nBUY: {f_coins(i.bz_price['buy'])}\nSELL: {f_coins(i.bz_price['sell'])}",
+                inline=False
+            )
+          
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def notify_me(self, ctx, item, above_below, price_per_item):
+        pass
+
+
+async def setup(bot):
+    await bot.add_cog(BazaarCog(bot))
--- __main__.py
+++ __main__.py
@@ -1,107 +1,44 @@
+#imports
 import discord
 from discord.ext import commands, tasks
-import math
-import requests
-import json
-import time
+import os
 import config
-import hypixel_api
-import random
+import asyncio
 
-hypixel = hypixel_api.HypixelApi()
-responses = config.RESPONSES
-print(responses)
+#Create bot variable
 intents = discord.Intents.default()
 intents.message_content = True
 prefix = config.GLOBAL_CONFIG["prefix"]
 bot = commands.Bot(command_prefix=prefix, intents=intents)
 
-bazaar_data = None
-
-def f_num(number, decimals=0):
-    return f"{round(number, decimals):,}"
-
-def f_coins(number):
-    return "$" + f_num(number, decimals=0)
-
+#setup cogs upon bot loading
+@bot.event
+async def setup_hook():
+    for filename in os.listdir("./Cogs"):
+            if filename.endswith(".py"):
+                await bot.load_extension(f'Cogs.{filename[:-3]}')
 @bot.event
 async def on_ready():
-    await bot.add_cog(BazaarCog(bot))
+     await bot.add_cog(Reload(bot))
+     print('loaded')
 
 
-class BazaarCog(commands.Cog):
+#Reload cogs command
+class Reload(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.settings = {
-            "margin": 250,
-            "volume": 1000000,
-        }
-        self.embed_list = ()
-
+     
     @commands.command()
-    async def bz(self, ctx, arg):
-        item = hypixel.items.get_item_by_id(arg)
-        embed = discord.Embed(
-            title=f"You wanted to know about {item.name} ~?",
-            color=0xF5A9B8
-        )
-        embed.set_thumbnail(url=bot.user.avatar.url)
-        embed.set_footer(text=time.ctime(time.time()))
-        embed.add_field(
-            name=item.name,
-            value=f"BUY: {f_coins(item.bz_price['buy'])}\nSELL: {f_coins(item.bz_price['sell'])}\n7d SALES: {f_num(item.bz_moving_week['sell'])}"
-        )
-        await ctx.send(embed=embed)
+    async def reload(self, ctx):
+        for filename in os.listdir("./Cogs"):
+            if filename.endswith(".py"):
+                await bot.unload_extension(f'Cogs.{filename[:-3]}')
+                await bot.load_extension(f'Cogs.{filename[:-3]}')
+        await ctx.send("https://tenor.com/view/aleks-workshop-reloading-reload-gif-20710655")
 
-    @commands.command()
-    async def margin(self, ctx, ammount, t_type):
-        if t_type == "percent":
-            ctx.send(f"Set margin tracker to {ammount}% of the total price.")
-        if t_type == "coins":
-            ctx.send(f"Set margin tracker to {ammount}% coins.")
-
-    @commands.command()
-    async def spike(self, ctx, ammount, t_type):
-        if t_type == "percent":
-            ctx.send(f"Set spike tracker to {ammount}% of the total price.")
-        if t_type == "coins":
-            ctx.send(f"Set spike tracker to {ammount}% coins.")
-
-    @commands.command()
-    async def sell_volume(self, ctx, ammount):
-        ctx.send(f"Set the tacker sell volume to {ammount} sellers.")
-
-    @commands.command()
-    async def buy_volume(self, ctx, ammount):
-        ctx.send(f"Set the tacker buy volume to {ammount} sellers.")
-
-    @commands.command()
-    async def brief(self, ctx):
-        new_search = hypixel.bazaar.limit_search()
-        new_search.add_limit("margin", False, self.settings["margin"])
-        new_search.add_limit("sellVolume", False, self.settings["volume"])
-        results = new_search.finalize()
-        embed = discord.Embed(
-            title=random.choice(responses),
-            color=0xF5A9B8,
-        )
-        embed.set_thumbnail(url=bot.user.avatar.url)
-        embed.set_footer(text=time.ctime(time.time()))
-
-        for i in results:
-
-            embed.add_field(
-                name=f"{i.name}",
-                value=f"MARGIN: {f_coins(i.bz_price['margin'])}\nBUY: {f_coins(i.bz_price['buy'])}\nSELL: {f_coins(i.bz_price['sell'])}",
-                inline=False
-            )
-            # await ctx.send(f"{i.name} has a margin of {i.bz_price['margin']:,} coins")
-            # await ctx.send(f"BUY: {i.bz_price['buy']} SELL: {i.bz_price['sell']}")
-        await ctx.send(embed=embed)
-
-    @commands.command()
-    async def notify_me(self, ctx, item, above_below, price_per_item):
-        pass
+                 
 
 
+
+#Run bot
 bot.run(config.GLOBAL_CONFIG["discord_token"])
