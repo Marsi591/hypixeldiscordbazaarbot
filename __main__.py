1acccedfd830ccbc42eb3e6892192cac37e2050f
creeperdr <drewday31@gmail.com>
2023-06-19 20:37:20 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,33 +1,37 @@
-#imports
+# imports
 import discord
 from discord.ext import commands, tasks
 import os
 import config
 import asyncio
 
-#Create bot variable
+# Create bot variable
 intents = discord.Intents.default()
 intents.message_content = True
 prefix = config.GLOBAL_CONFIG["prefix"]
 bot = commands.Bot(command_prefix=prefix, intents=intents)
 
-#setup cogs upon bot loading
+# setup cogs upon bot loading
+
+
 @bot.event
 async def setup_hook():
     for filename in os.listdir("./Cogs"):
-            if filename.endswith(".py"):
-                await bot.load_extension(f'Cogs.{filename[:-3]}')
+        if filename.endswith(".py"):
+            await bot.load_extension(f'Cogs.{filename[:-3]}')
+
+
 @bot.event
 async def on_ready():
-     await bot.add_cog(Reload(bot))
-     print('loaded')
+    await bot.add_cog(Reload(bot))
+    print('loaded')
 
 
-#Reload cogs command
+# Reload cogs command
 class Reload(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-     
+
     @commands.command()
     async def reload(self, ctx):
         for filename in os.listdir("./Cogs"):
@@ -36,9 +40,11 @@ class Reload(commands.Cog):
                 await bot.load_extension(f'Cogs.{filename[:-3]}')
         await ctx.send("https://tenor.com/view/aleks-workshop-reloading-reload-gif-20710655")
 
-                 
 
+# Loads all of the commands from the "Cogs" folder
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs{filename[:-3]}')
 
-
-#Run bot
+# Run bot
 bot.run(config.GLOBAL_CONFIG["discord_token"])
